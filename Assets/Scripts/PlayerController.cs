81588f2a8a423d5721b6dcfa55ff259b44342421
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-06-23 18:37:58 +0900

    initial commit (1st week)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// シューティングゲームの自機を操作するためのコンポーネント
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>プレイヤーの移動速度</summary>
+    [SerializeField] float m_moveSpeed = 5f;
+    /// <summary>弾のプレハブ</summary>
+    [SerializeField] GameObject m_bulletPrefab;
+    Rigidbody2D m_rb2d;
+
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 自機を移動させる
+        float h = Input.GetAxisRaw("Horizontal");   // 垂直方向の入力を取得する
+        float v = Input.GetAxisRaw("Vertical");     // 水平方向の入力を取得する
+        Vector2 dir = new Vector2(h, v).normalized; // 進行方向の単位ベクトルを作る (dir = direction) 
+        m_rb2d.velocity = dir * m_moveSpeed;        // 単位ベクトルにスピードをかけて速度ベクトルにして、それを Rigidbody の速度ベクトルとしてセットする
+
+        // 左クリックまたは左 Ctrl で弾を発射する
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (m_bulletPrefab) // m_bulletPrefab にプレハブが設定されている時
+            {
+                Instantiate(m_bulletPrefab, this.transform.position, m_bulletPrefab.transform.rotation);  // インスペクターから設定した m_bulletPrefab をインスタンス化する
+            }
+        }
+    }
+}
